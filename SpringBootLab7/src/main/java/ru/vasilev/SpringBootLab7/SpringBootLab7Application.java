1f7cf8731ba12a4240815764525eb3d4b9d968ce
Vasilev0009 <ben00000009@gmail.com>
2024-01-23 13:35:46 +0500

    End

---
+++ SpringBootLab7/src/main/java/ru/vasilev/SpringBootLab7/SpringBootLab7Application.java
@@ -0,0 +1,13 @@
+package ru.vasilev.SpringBootLab7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLab7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLab7Application.class, args);
+	}
+
+}
---
+++ SpringBootLab7/src/test/java/ru/vasilev/SpringBootLab7/SpringBootLab7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vasilev.SpringBootLab7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLab7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
